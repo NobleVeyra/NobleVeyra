ce6cee0cee11a36a273bf0964c26c7d6c7bb008f
NobleVeyra <nobleveyraadvisory@gmail.com>
2026-09-06 00:19:17 -0500

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -2,7 +2,7 @@ window.NOBLEVEYRA = {
   strategyPaymentUrl: "https://buy.stripe.com/8x2bJ10Cpgrhag6ct64Rq01",
   intensiveApplicationUrl: "https://buy.stripe.com/cNi9ATcl7b6Xag6bp24Rq00",
   bookingUrl: "https://calendar.app.google/dReJnHRUXnpXrDiV9",
-  instagramUrl: "https://www.instagram.com/nobleveyraadvisory?stkn=bTNjeWptdTBiNml5&utm_source=qr",
-  threadsUrl: "https://www.threads.com/@noble_tj?igshid=NTc4MTIwNjQ2YQ==",
+  instagramUrl: "https://www.instagram.com/nobleveyraadvisory",
+  threadsUrl: "https://www.threads.com/@nobleveyraadvisory",
   email: "tony@nobleveyraadvisory.com"
 };
